5ff5ee70a6230e9ac47e22798d066b8767021ec2
pranavranganath <155946238+pranavranganath@users.noreply.github.com>
2024-02-24 18:59:26 +0530

    Create 15.java

---
+++ strings/15.java
@@ -0,0 +1,16 @@
+public class strings15 {
+    
+        public static void main(String[] args) {
+            // Original string
+            String originalString = "I go to College.";
+    
+            // Replacing "College" with "Commege"
+            String replacedString = originalString.replace("College", "Commege");
+    
+            // Printing the original and replaced strings
+            System.out.println("Original String: " + originalString);
+            System.out.println("Replaced String: " + replacedString);
+        }
+    }
+    
+
