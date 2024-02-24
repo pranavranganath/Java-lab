58acff2d1f5911e9a240cf4764ca553ced3e232d
pranavranganath <155946238+pranavranganath@users.noreply.github.com>
2024-02-24 18:59:04 +0530

    Create 14.java

---
+++ strings/14.java
@@ -0,0 +1,8 @@
+public class strings14 {
+    public static void main(String[] args) {
+        String s1="Hello";
+        String s2="World";
+        String s3=s1.concat(s2);
+        System.out.println(s3);
+    }
+}
